e4330dc94f28dcae688ce23afb5eaea8f417a811
Katherine Pully <kpully@gmail.com>
2016-06-09 19:42:00 -0400

    advanced python csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,6 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+dir = '/Users/kpully/ds/metis/prework/dsp/python/faculty.csv'
+data = pd.read_csv(dir)
+df = pd.DataFrame(data)
+df.columns = ['name', 'degree', 'title', 'email']
+df.email.to_csv('emails.csv')
